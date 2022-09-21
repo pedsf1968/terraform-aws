699751934762b65edb877d8a38af4bd43c207ba1
terraform <terraform@isa-server>
2022-09-21 19:22:30 +0200

    first commit for aws provider

---
+++ aws_iam_policy_json/main.tf
@@ -0,0 +1,16 @@
+resource "aws_iam_user" "admin-user" {
+  name = "lucy"
+  tags = {
+    Description = "Technical Team Leader"
+  }
+}
+
+resource "aws_iam_policy" "adminUser" {
+  name   = "AdminUsers"
+  policy = file("admin-policy.json")
+}
+
+resource "aws_iam_user_policy_attachment" "lucy-admin-access" {
+  user       = aws_iam_user.admin-user.name
+  policy_arn = aws_iam_policy.adminUser.arn
+}
